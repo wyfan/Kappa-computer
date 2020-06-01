49cac144b14f9c6664ff3c1d4a87e8d1311120eb
Pulipuli Chen <blog@pulipuli.info>
2020-06-01 16:08:26 +0800

    0601 mnist完成

--- scripts/vue-app-watch.js
+++ scripts/vue-app-watch.js
@@ -2,12 +2,23 @@ var appWatch = {
   selectFileNameList () {
     
     let maxLength = 3
-    let shortNames = this.classList.map(name => {
+    let shortNames = []
+    this.classList.forEach(name => {
+      //console.log(name, this.unknownClassName)
+      if (name === this.unknownClassName) {
+        return false
+      }
+      
       if (name.length > maxLength) {
         name = name.slice(0, maxLength)
       }
-      return name
+      
+      if (shortNames.indexOf(name) === -1) {
+        return shortNames.push(name)
+      }
     })
+    
+    // 過濾掉重複的
     
     this.filename = 'images-' + shortNames.join('_') + '-' + (new Date()).mmddhhmm()
     this.filenamePlaceholder = this.filename
